a33045c2ee3ef2e49102c5528d8fd9818cf2bf3a
alexefthymioutes <alexefthymiou@gmail.com>
2026-04-14 13:37:34 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,17 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  app: {
+    head: {
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon/favicon.ico" },
+        { rel: "icon", type: "image/svg+xml", href: "/favicon/favicon.svg" },
+        { rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon/favicon-96x96.png" },
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png" },
+        { rel: "manifest", href: "/favicon/site.webmanifest" },
+      ],
+    },
+  },
   modules: ["@nuxtjs/sitemap", "@nuxtjs/robots"],
   site: {
     url: "https://my-glass.co.uk",
